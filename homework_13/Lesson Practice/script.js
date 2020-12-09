887b72eafff9552aa9d2def9b4dffd86967638b2
Ktjucha <ktjucha91@gmail.com>
2020-12-09 10:12:18 +0300

    09.12.2020

---
+++ homework_13/Lesson Practice/script.js
@@ -0,0 +1,30 @@
+var container = document.getElementById('container');
+
+var firstPar = document.createElement('p'),
+    secondPar = document.createElement('p');
+
+firstPar.innerHTML = 'Hello, here are <a href="https://www.facebook.com">Link 1</a> and <a href="https://twitter.com">Link 2</a>';
+secondPar.innerHTML = 'Hello, here are <a href="http://google.by">Link 3</a> and <a href="https://vk.com">Link 4</a>';
+
+container.appendChild(firstPar);
+container.appendChild(secondPar);
+
+var buttClick = document.body.getElementsByTagName('button');
+
+buttClick[0].addEventListener('click', function() {
+    var childrenFirst = firstPar.children;
+
+    for (var i = 0; i < childrenFirst.length; i++) {
+        childrenFirst[i].className = 'redClass';
+        }
+});
+
+var childrenSecond = secondPar.children;
+
+childrenSecond[0].onclick = myAlert;
+childrenSecond[1].onclick = myAlert;
+
+function myAlert(EO) {
+    event.preventDefault();
+    alert(this.href);
+}
---
+++ homework_13/script.js
@@ -0,0 +1,50 @@
+var tableContainer = document.getElementsByTagName('tbody'),
+    idContainer = document.getElementById('add');
+
+idContainer.addEventListener("click", function(event) {
+    var tdOld = document.getElementsByTagName('tr'),
+        row = document.createElement("tr"),
+        td1 = document.createElement("td"),
+        td2 = document.createElement("td"),
+        td3 = document.createElement("td");
+
+    tableContainer[0].insertBefore(row, tdOld[0]);
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+});
+
+
+
+tableContainer[0].addEventListener("click", function(event) {
+    var target = event.target;
+
+    if (target.parentElement == idContainer){
+      return;
+    }
+
+    if (target.textContent == '') {
+        target.innerHTML = '<input type="text" size="10" id="inputId">';
+        target.firstElementChild.focus();
+    } else{
+        var str = target.textContent;
+
+        target.innerHTML = '<input type="text" size="10" id="inputId">';
+
+        var inputElement = document.getElementById('inputId');
+
+        inputElement.value += str;
+        inputElement.focus();
+
+    }
+
+    var input = document.getElementById('inputId');
+
+    input.addEventListener("focusout", function() {
+        target.textContent = input.value;
+        input.remove();
+    });
+
+});
+
+
