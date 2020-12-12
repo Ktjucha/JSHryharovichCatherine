414e0a30940e3586e1bab91b5c909bc5576c2bf8
Ktjucha <ktjucha91@gmail.com>
2020-12-12 23:36:40 +0300

    12.12.2020

--- homework_13/Lesson Practice/script.js
+++ homework_13/Lesson Practice/script.js
@@ -2,6 +2,7 @@ var container = document.getElementById('container');
 
 var firstPar = document.createElement('p'),
     secondPar = document.createElement('p');
+localStorage.clear();
 
 firstPar.innerHTML = 'Hello, here are <a href="https://www.facebook.com">Link 1</a> and <a href="https://twitter.com">Link 2</a>';
 secondPar.innerHTML = 'Hello, here are <a href="http://google.by">Link 3</a> and <a href="https://vk.com">Link 4</a>';
@@ -23,6 +24,18 @@ secondPar.onclick = myAlert;
 
 function myAlert(event) {
     var target = event.target;
-    event.preventDefault();
-    alert(target.href);
+
+    if(target == secondPar.children[0] || target == secondPar.children[1]) {
+        event.preventDefault();
+
+        if(localStorage.getItem(target.href)){
+            alert(target.href);
+        }else{
+            localStorage.setItem('path', target.href);
+            var targetSave = target.href;
+            target.href = "#";
+            alert('ссылка '+ targetSave +' сохранена');
+        }
+
+    }
 }
---
+++ homework_14/script.js
@@ -0,0 +1,81 @@
+var coordinats = document.getElementsByTagName('input'),
+    button = document.getElementsByTagName('button');
+
+button[0].setAttribute("disabled", "true");
+
+function logKey(){
+    if (coordinats[0].value != '' && coordinats[1].value != '') {
+        button[0].removeAttribute("disabled");
+    } else {
+        button[0].setAttribute("disabled", "true");
+    }
+}
+
+coordinats[0].addEventListener('keyup', logKey);
+coordinats[1].addEventListener('keyup', logKey);
+button[0].addEventListener('click', inputValidation);
+
+function inputValidation() {
+
+    var inputX = +coordinats[0].value,
+        inputY = +coordinats[1].value;
+
+    if (isNaN(inputX) || (inputX ^ 0) !== inputX || inputX < 1 || inputX > 10) {
+        alert('Ошибка в поле X, введите целое число от 1 до 10');
+        coordinats[0].value = '';
+        return;
+    }
+
+    if (isNaN(inputY) || (inputY ^ 0) !== inputY || inputY < 1 || inputY > 10) {
+        alert('Ошибка в поле Y, введите целое число от 1 до 10');
+        coordinats[1].value = '';
+        return;
+        }
+
+    if (document.body.children[4]) {
+        document.body.children[4].remove();
+    }
+
+    var table = document.createElement('table');
+    document.body.appendChild(table);
+
+    for (var i = 0; i < inputY; i++) {
+        var trContainer = document.createElement('tr');
+
+        table.appendChild(trContainer);
+
+        for (var j = 0; j < inputX; j++) {
+            var tdContainer = document.createElement('td');
+
+            trContainer.appendChild(tdContainer);
+        }
+    }
+
+    button[0].setAttribute("disabled", "true");
+    coordinats[0].value = '';
+    coordinats[1].value = '';
+    coordinats[0].addEventListener('keyup', logKey);
+    coordinats[1].addEventListener('keyup', logKey);
+
+    var tableContainer = document.getElementsByTagName('table');
+
+    tableContainer[0].addEventListener('click', clickTable);
+
+    function clickTable() {
+        var tdColor = document.getElementsByTagName('td');
+
+        for(var i = 0; i < tdColor.length; i++){
+
+            if(window.getComputedStyle(tdColor[i] ,null).getPropertyValue('background-color') === 'rgb(0, 0, 0)'){
+                tdColor[i].style.background = '#ffffff';
+            }else{
+                tdColor[i].style.background = '#000000';
+            }
+        }
+
+    }
+}
+
+
+
+
